c5641039a1303612958ae416ee2085e30d432434
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-10 12:07:21 +0200

    Create swimming.js

---
+++ swimming.js
@@ -0,0 +1,74 @@
+const swimmingHallData = [
+	{
+		swimmingHallName: 'Matinkylän uimahalli',
+		relatedLinks: [
+			{
+				relatedLinkName: 'Terapia-allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=24790',
+			},
+			{
+				relatedLinkName: 'Kuntosali',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=24847',
+			},
+			{
+				relatedLinkName: '50m, koko allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=24767',
+			},
+		],
+	},
+	{
+		swimmingHallName: 'Leppävaaran uimahalli',
+		relatedLinks: [
+			{
+				relatedLinkName: 'Terapia-allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=18509',
+			},
+			{
+				relatedLinkName: 'Kuntosali',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16083',
+			},
+			{
+				relatedLinkName: 'Maauimala, koko allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=19191',
+			},
+			{
+				relatedLinkName: 'Iso allas, koko allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16074',
+			},
+		],
+	},
+	{
+		swimmingHallName: 'Espoonlahden uimahalli',
+		relatedLinks: [
+			{
+				relatedLinkName: 'Kuntosali 1 (yläsali)',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16765',
+			},
+			{
+				relatedLinkName: 'Vapaaharjoittelukuntosali',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=15958',
+			},
+			{
+				relatedLinkName: '50 m, koko allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=15939',
+			},
+		],
+	},
+	{
+		swimmingHallName: 'Keski-Espoon uimahalli',
+		relatedLinks: [
+			{
+				relatedLinkName: 'Terapia-allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16650',
+			},
+			{
+				relatedLinkName: 'Kuntosali',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16654',
+			},
+			{
+				relatedLinkName: 'Iso allas, koko allas',
+				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16645',
+			},
+		],
+	},
+];
