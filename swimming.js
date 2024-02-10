6b26741e238e97e72ac0987c94e87058e5774dc3
Krugou <59641229+Krugou@users.noreply.github.com>
2024-02-10 13:39:36 +0200

    Add fetch API to retrieve data from external source

--- swimming.js
+++ swimming.js
@@ -1,24 +1,25 @@
 // https://resurssivaraus.espoo.fi/Tailored/prime_product_intranet/espoo/web/Calendar/ReservationData.aspx?resourceid%5B%5D=16650&start=1707084000&end=1707688800&_=1707560390539
-
+const reservationUrl =
+	'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=';
 const swimmingHallData = [
 	{
 		swimmingHallName: 'Matinkylän uimahalli',
 		relatedLinks: [
 			{
 				relatedLinkName: 'Swim',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=24767,24790',
+				url: '24767,24790',
 			},
 			{
 				relatedLinkName: 'Terapia-allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=24790',
+				url: '24790',
 			},
 			{
 				relatedLinkName: 'Kuntosali',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=24847',
+				url: '24847',
 			},
 			{
 				relatedLinkName: '50m, koko allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=24767',
+				url: '24767',
 			},
 		],
 	},
@@ -27,27 +28,27 @@ const swimmingHallData = [
 		relatedLinks: [
 			{
 				relatedLinkName: 'Swim',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=18509,16074',
+				url: '18509,16074',
 			},
 			{
 				relatedLinkName: 'Swim(summer)',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=18509,19191,16074',
+				url: '18509,19191,16074',
 			},
 			{
 				relatedLinkName: 'Terapia-allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=18509',
+				url: '18509',
 			},
 			{
 				relatedLinkName: 'Kuntosali',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16083',
+				url: '16083',
 			},
 			{
 				relatedLinkName: 'Maauimala, koko allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=19191',
+				url: '19191',
 			},
 			{
 				relatedLinkName: 'Iso allas, koko allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16074',
+				url: '16074',
 			},
 		],
 	},
@@ -56,15 +57,15 @@ const swimmingHallData = [
 		relatedLinks: [
 			{
 				relatedLinkName: 'Kuntosali 1 (yläsali)',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16765',
+				url: '16765',
 			},
 			{
 				relatedLinkName: 'Vapaaharjoittelukuntosali',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=15958',
+				url: '15958',
 			},
 			{
 				relatedLinkName: '50 m, koko allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=15939',
+				url: '15939',
 			},
 		],
 	},
@@ -73,19 +74,19 @@ const swimmingHallData = [
 		relatedLinks: [
 			{
 				relatedLinkName: 'Swim',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16650,16645',
+				url: '16650,16645',
 			},
 			{
 				relatedLinkName: 'Terapia-allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16650',
+				url: '16650',
 			},
 			{
 				relatedLinkName: 'Kuntosali',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16654',
+				url: '16654',
 			},
 			{
 				relatedLinkName: 'Iso allas, koko allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=16645',
+				url: '16645',
 			},
 		],
 	},
@@ -94,11 +95,11 @@ const swimmingHallData = [
 		relatedLinks: [
 			{
 				relatedLinkName: 'Koko allas',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=15968',
+				url: '15968',
 			},
 			{
 				relatedLinkName: 'Kuntosali',
-				url: 'https://resurssivaraus.espoo.fi/liikunnantilavaraus/haku/?ResourceIDs=15967',
+				url: '15967',
 			},
 		],
 	},
